4ce4be886d93bfe4eb002da1d715baa9369ec866
jawaluke <jawharlakshmanan6@gmail.com>
2022-04-10 18:13:42 +0530

    added cors config class

--- src/main/java/com/flizzerui22/chat22/config/WebSocketConfig.java
+++ src/main/java/com/flizzerui22/chat22/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.flizzerui22.chat22.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
