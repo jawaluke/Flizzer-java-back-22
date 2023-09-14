f211d67f33bdcbc64d518712e0d54c838c4b303b
Jawahar lakshmanan <52790465+jawaluke@users.noreply.github.com>
2023-09-14 12:14:27 +0530

    added log and system message
    Co-authored-by: jawaluke <jawharlakshmanan6@gmail.com>

--- src/main/java/com/flizzerui22/chat22/controller/ChatController.java
+++ src/main/java/com/flizzerui22/chat22/controller/ChatController.java
@@ -27,6 +27,7 @@ public class ChatController {
     @GetMapping("/info")
     public String greetInfo(){
         System.out.println("Greetings page");
+        log.info("user entered to welcome page");
         return "Welcome to flizzer chatUI22";
     }    
 
